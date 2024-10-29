377ad77d62d1969de9bad7052383943b4ab2dd60
fatihua <fatihua@malangkab.go.id>
2024-10-29 16:02:36 +0700

    feat: update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,5 @@
 import { auth } from "@/auth";
+import { notFound } from "next/navigation";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
@@ -19,40 +20,22 @@ export default auth((req: NextRequest) => {
       ?.replace(".localhost:3000", `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`) ??
     "";
 
-  // console.log("isLoggedIn:", isLoggedIn, "Hostname:", hostname);
+  const customSubDomain = req.headers
+    .get("host")
+    ?.split(`.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`)[0]; // Mendapatkan bagian sebelum domain utama
 
-  // Pengalihan ke /app jika hostname sesuai
-  if (hostname === `app.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`) {
-    if (!isLoggedIn && nextUrl.pathname !== "/login") {
-      console.log("NOT LOGIN");
-      return NextResponse.redirect(new URL("/login", req.url));
-    } else if (isLoggedIn && nextUrl.pathname == "/login") {
-      console.log("LOGIN");
-      return NextResponse.redirect(new URL("/app", req.url));
-    }
-    return NextResponse.rewrite(
-      new URL(
-        `/app${nextUrl.pathname === "/" ? "" : nextUrl.pathname}`,
-        req.url
-      )
-    );
+  // Daftar subdomain yang diizinkan tanpa pengecekan auth
+  const allowedSubdomains = ["test1", "test2"];
+  // console.log(customSubDomain);
+  if (publicUrls.includes(currentUrl)) {
+    return NextResponse.next();
   }
 
-  // Pengalihan ke /app jika hostname sesuai
-  if (hostname === `cockpit.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`) {
-    return NextResponse.rewrite(
-      new URL(
-        `/cockpit${nextUrl.pathname === "/" ? "" : nextUrl.pathname}`,
-        req.url
-      )
-    );
-  }
-
-  // Pengalihan ke /app jika hostname sesuai
   if (
     hostname === "localhost:3000" ||
     hostname === process.env.NEXT_PUBLIC_ROOT_DOMAIN
   ) {
+    // Pengalihan ke /site untuk domain utama
     return NextResponse.rewrite(
       new URL(
         `/site${nextUrl.pathname === "/" ? "" : nextUrl.pathname}`,
@@ -61,26 +44,55 @@ export default auth((req: NextRequest) => {
     );
   }
 
-  // Izinkan akses ke rute publik tanpa pengalihan
-  if (publicUrls.includes(currentUrl)) {
-    return NextResponse.next();
+  // Cek apakah customSubDomain ada
+  if (customSubDomain) {
+    if (customSubDomain === "app") {
+      // Pengalihan ke /app dengan pengecekan login
+      if (!isLoggedIn && nextUrl.pathname !== "/login") {
+        console.log("NOT LOGIN");
+        return NextResponse.redirect(new URL("/login", req.url));
+      } else if (isLoggedIn && nextUrl.pathname === "/login") {
+        console.log("LOGIN");
+        return NextResponse.redirect(new URL("/app", req.url));
+      }
+      return NextResponse.rewrite(
+        new URL(
+          `/app${nextUrl.pathname === "/" ? "" : nextUrl.pathname}`,
+          req.url
+        )
+      );
+    } else if (customSubDomain === "cockpit") {
+      // Pengalihan ke /cockpit tanpa pengecekan login
+      if (!isLoggedIn && nextUrl.pathname !== "/login") {
+        console.log("NOT LOGIN");
+        return NextResponse.redirect(new URL("/login", req.url));
+      } else if (isLoggedIn && nextUrl.pathname === "/login") {
+        console.log("LOGIN");
+        return NextResponse.redirect(new URL("/cockpit", req.url));
+      }
+      return NextResponse.rewrite(
+        new URL(
+          `/cockpit${nextUrl.pathname === "/" ? "" : nextUrl.pathname}`,
+          req.url
+        )
+      );
+    } else if (allowedSubdomains.includes(customSubDomain)) {
+      // Pengalihan ke rute dinamis /[domain]/[slug] untuk subdomain yang ada di array allowedSubdomains
+      console.log("domain", customSubDomain);
+      return NextResponse.rewrite(
+        new URL(`/${customSubDomain}${nextUrl.pathname}`, req.url)
+      );
+    } else {
+      // Jika customSubDomain tidak ada dalam daftar yang diizinkan, return 404
+      console.log("Subdomain tidak dikenal, return 404");
+      return NextResponse.rewrite(new URL("/not-found", req.url));
+      // return notFound();
+    }
   }
 
-  // Pengalihan pengguna yang sudah login dari halaman otentikasi ke halaman utama
-  // if (isLoggedIn && authUrls.includes(currentUrl)) {
-  //   return NextResponse.redirect(new URL("/", req.url));
-  // }
-
-  // // Pengalihan pengguna yang belum login dari rute yang dilindungi ke halaman login
-  // if (
-  //   !isLoggedIn &&
-  //   !publicUrls.includes(currentUrl) &&
-  //   !authUrls.includes(currentUrl)
-  // ) {
-  //   return NextResponse.redirect(new URL("/login", req.url));
-  // }
-
-  return NextResponse.next();
+  // Jika tidak memenuhi kondisi mana pun, return 404
+  console.log("Hostname atau subdomain tidak dikenali, return 404");
+  return NextResponse.rewrite(new URL("/not-found", req.url));
 });
 
 export const config = {
